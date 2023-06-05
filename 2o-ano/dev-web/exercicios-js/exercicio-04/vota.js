2a70568eb48d8b7b1c1c9d04a65f2d5795613c53
Matheus Pompeo <matheuspompeo13@gmail.com>
2023-06-05 07:37:02 -0300

    Add files via upload

---
+++ 2o-ano/dev-web/exercicios-js/exercicio-04/vota.js
@@ -0,0 +1,14 @@
+function calculo () {
+    let idade = Number(document.getElementById('idade').value)
+    let nome = document.getElementById('nome')
+    console.log('idade')
+    console.log('nome')
+
+    if (idade => 18) {
+        
+    } else {
+        
+    }
+
+
+}
