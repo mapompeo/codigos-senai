90f440a8f72af6be30c0cd7ee9bf38420c6bd47b
Matheus Pompeo <matheuspompeo13@gmail.com>
2023-06-29 09:21:51 -0300

    correções no código da prova

--- 2o-ano/dev-web/prova-27-06-23/questao-03/script.js
+++ 2o-ano/dev-web/prova-27-06-23/questao-03/script.js
@@ -1,29 +1,37 @@
-let vetor = [0, 0, 0, 0, 0]
-let tresEcinco = 0
-let tres = 0
-let cinco = 0
-vetorHTML = document.getElementById("vetorHTML").value
+let vetor = []
+let div3e5 = 0
+let div3 = 0
+let div5 = 0
+let i = 0
 
 function calculo() {
-    let valores = Number(document.getElementById("valores").value)
-    
-    vetor.push(valores)
+    let valor = Number(document.getElementById("valores").value)
 
-    vetorHTML.innerHTML = `Valores: ${vetor[0]}, ${vetor[1]}, ${vetor[2]}, ${vetor[3]}, ${vetor[4]}.`
-    for (let i = 0; i < 5; i++) {
-
-        if (vetor[i] % 3 == 0 && vetor[i] % 5 == 0) {
-            tresEcinco = tresEcinco + vetor[i]
-        }
-        else if (vetor[i] % 3 == 0) {
-            tres = tres + vetor[i]
-        }
-        else {
-            cinco = cinco + vetor[i]
-        }
-        i++;
-
-        console.log("vetor")
+    if(i < 5){
+        vetor[i] = valor
     }
-    
+
+    else if (i == 5){  
+        for(indice = 0; indice < 5; indice++){
+
+            if(vetor[indice] % 3 == 0 && vetor[indice] % 5 == 0){
+            div3e5 = div3e5 + 1          
+            }
+
+            if(vetor[indice] % 3 == 0){
+                div3 = div3 + 1          
+            }
+            if(vetor[indice] % 5 == 0){
+                div5 = div5 + 1          
+            }
+
+        }
+    div3e5HTML.innerHTML = `${div3e5} Números são divisíveis por 3 e 5.`
+    div3HTML.innerHTML = `${div3} Números são divisíveis por 3.`
+    div5HTML.innerHTML = `${div5} Números são divisíveis por 5.`
+    }
+
+    i++;
+
+    vetorHTML.innerHTML = `Valores: ${vetor.join(", ")}.`
 }
