d7bd01b6ac27ef34826d9d4c5ff5b00e916e428a
Matheus Pompeo <matheuspompeo13@gmail.com>
2023-08-14 08:00:34 -0300

    atualização

---
+++ 2o-ano/fundamento-de-redes/Situação de aprendizagem/exercicio-09/script.js
@@ -0,0 +1,25 @@
+let res = document.getElementById('res')
+
+function binario(){
+    let numero = document.getElementById('numero').value
+    let cbinario = parseInt(numero, 10)
+    let final = cbinario.toString(2)
+
+    res.innerHTML = `O número ${numero} em binário fica ${final}`
+}
+
+function octal(){
+    let numero = document.getElementById('numero').value
+    let coctal = parseInt(numero, 10)
+    let final = coctal.toString(8)
+
+    res.innerHTML = `O número ${numero} em binário fica ${final}`
+}
+
+function hexadecimal(){
+    let numero = document.getElementById('numero').value
+    let coctal = parseInt(numero, 10)
+    let final = coctal.toString(16)
+
+    res.innerHTML = `O número ${numero} em binário fica ${final}`
+}
