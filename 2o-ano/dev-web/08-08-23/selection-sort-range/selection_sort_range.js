c3141434c2c62e40b0b76abd67b3abacb549b18c
Matheus Pompeo <matheuspompeo13@gmail.com>
2023-08-22 09:42:24 -0300

    atualizações

--- 2o-ano/dev-web/08-08-23/selection-sort-range/selection_sort_range.js
+++ 2o-ano/dev-web/08-08-23/selection-sort-range/selection_sort_range.js
@@ -1,29 +1,39 @@
 let res = document.getElementById('res')
 let res2 = document.getElementById('res2')
-
 let num_array = []
-let minimo = 0
 
-function randomize(min, max) {
-    let valor = 0
-    valor = Math.floor(Math.random() * (max - min + 1)) + min
-
-    console.log(valor)
-    return valor
+function randomize(minimo, maximo) {
+    let resultadoRandom = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo
+    return resultadoRandom
 }
 
 function inserir() {
-    let insert = Number(document.getElementById('insert').value)
-    let maximo = insert
-    let resposta = 0
+    num_array = []
+    let minimo = 0
+    let maximo = Number(document.getElementById('insert').value)
 
-    for (let index = 0; index < 5; index++) {
-        randomize(minimo, maximo)
-        num_array.push(resposta)
+    for (let index = minimo; index < maximo; index++) {
+        let resultado = randomize(minimo, maximo)
+        num_array.push(resultado)
     }
-    console.log(`Resposta = ${resposta}`)
+    res.innerHTML = `Os ${maximo} números aleatórios são = ${num_array.join(', ')}.`
 }
 
 function ordenar() {
-    
+    let valor_atual = 0
+
+    for (let index = 0; index < num_array.length; index++) {
+        let menor = index
+        for (j = index + 1; j < num_array.length; j++){
+            if (num_array[j] < num_array[menor]){
+                menor = j
+            }
+        }
+        if (i != menor) {
+            valor_atual = num_array[i]
+            num_array[i] = num_array[menor]
+            num_array[menor] = valor_atual
+        }
+    }
+    res2.innerHTML = `Os valores ordenados por ordem decrescente utilizando o método "selection.sort" são: ${num_array}.`
 }
