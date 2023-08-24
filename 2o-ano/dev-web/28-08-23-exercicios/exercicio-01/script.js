0683c5642664411dd0575b6365b81ea11ac4e794
Matheus Pompeo <matheuspompeo13@gmail.com>
2023-08-24 13:48:35 -0300

    atualizações

--- 2o-ano/dev-web/28-08-23-exercicios/exercicio-01/script.js
+++ 2o-ano/dev-web/28-08-23-exercicios/exercicio-01/script.js
@@ -4,27 +4,18 @@ let array = []
 
 function aleatorio(){
     array = []
-
     for (let index = 0; index < 10; index++) {
-        let resultado = Math.random() * (10 - 0)
+        let resultado = Math.floor(Math.random() * (10 - 0))
         array.push(resultado)
     }
-
-    // Código pego na internet para arredondar os valores do vetor para uma casa decimal depois da vírgula
-    let decimalPlaces = 1;
-    let fixedArray = array.map(value => Number(value.toFixed(decimalPlaces)));
-    // --
-
-    valoresAleatorio.innerHTML = `Os valores aleatórios são: ${fixedArray.join(', ')}.`
+    valoresAleatorio.innerHTML = `Os valores aleatórios são: ${array.join(', ')}.`
 }
 
 function media() {
     let soma = 0
-
     for (let index = 0; index < array.length; index++) {
         soma += array[index]
     }
     let resultado = soma / 10
-
-    resultadoMedia.innerHTML = `A média dos valores é: ${resultado.toFixed(2)}.`
+    resultadoMedia.innerHTML = `A média dos valores é: ${resultado}.`
 }
--- 2o-ano/dev-web/28-08-23-exercicios/exercicio-02/script.js
+++ 2o-ano/dev-web/28-08-23-exercicios/exercicio-02/script.js
@@ -1,29 +1,23 @@
 let resultadoAleatorio = document.getElementById('resultadoAleatorio')
 let resultadoPares = document.getElementById('resultadoPares')
 let array = []
-let arrayFixado = []
 
 function aleatorio() {
     array = [];
     for (let index = 0; index < 10; index++) {
-        let resultado = Math.random() * (20 - 0)
+        let resultado = Math.floor(Math.random() * (20 - 0))
+        Math.floor(resultado)
         array.push(resultado)
     }
-    arrayFixado = array.map(value => Number(value.toFixed(2))) 
-
-    resultadoAleatorio.innerHTML = `Os valores aleatórios gerados são: ${arrayFixado.join(', ')}.`
-
-    return arrayFixado
+    resultadoAleatorio.innerHTML = `Os valores aleatórios gerados são: ${array.join(', ')}.`
 }
 
-function pares(arrayFixado) {
+function pares() {
     let somaPares = 0
-
-    for (let index = 0; index < arrayFixado.length; index++) {
-        if (arrayFixado[index] % 2 == 0) {
+    for (let index = 0; index < array.length; index++) {
+        if (array[index] % 2 == 0) {
             somaPares++
         }
     }
-
     resultadoPares.innerHTML = `A quantidade de valores pares é ${somaPares}`
 }
