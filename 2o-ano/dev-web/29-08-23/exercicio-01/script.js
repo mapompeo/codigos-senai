ff6e9c11f6beff2d0afa1da8ce061d77a987a962
Matheus Pompeo <matheuspompeo13@gmail.com>
2023-08-29 08:42:44 -0300

    exercicio 01 e exercicio 02 realizados

---
+++ 2o-ano/dev-web/29-08-23/exercicio-01/script.js
@@ -0,0 +1,37 @@
+let array = []
+let soma = 0
+
+function apertado() {
+    if (event.key === 'Enter') {
+      inserir();
+    }
+}
+
+function inserir() {
+    let input = Number(document.getElementById('input').value)
+    if (input >= 0 && input <= 10) {
+        if (array.length < 6) {
+        array.push(input)
+        // soma = soma/array.length
+        document.getElementById('arrayResposta').innerHTML = `Os valores inseridos são ${array.join(', ')}.`
+        }
+        else {
+            document.getElementById('erro').innerHTML = `Os valores máximos permitidos são até 6! Clique no próximo botão.`
+        }
+    }
+    else {
+        document.getElementById('erro').innerHTML = `Insira valores entre 0 e 10!`
+    }
+}
+
+function media() {
+    // let resultado = soma/array.length
+    // mediaResposta.innerHTML = `A média dos valores é: ${resultado.toFixed(2)}!`
+    for (let i = 0; i < array.length; i++) {
+        soma = soma + array[i]
+    }
+    soma = soma/array.length
+    document.getElementById('mediaResposta').innerHTML = `A média dos valores é: ${soma.toFixed(2)}!`
+}
+
+// no código existem dois modos de fazer a média dos valores, um método está sendo usado e o outro está comentado!!
---
+++ 2o-ano/dev-web/29-08-23/exercicio-02/script.js
@@ -0,0 +1,17 @@
+let array = 0
+
+function gerar() {
+    array = []
+    for (let i = 0; i < 5; i++){
+        array.push(Math.floor(Math.random() * (10 - 0 + 1) + 0))
+    }
+    document.getElementById('arrayResposta').innerHTML = `Os valores aleatórios gerados no array são: ${array.join(', ')}.`
+}
+
+function adicionar() {
+    soma = 0
+    for (let i = 0; i < array.length; i++) {
+        soma += array[i]
+    }
+    document.getElementById('mediaResposta').innerHTML = `A média dos valores é: ${soma.toFixed(2)}!`
+}
