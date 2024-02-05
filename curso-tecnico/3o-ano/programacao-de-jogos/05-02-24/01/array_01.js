ed8f44acc08d684140b0b88ad9b80f192852786a
Matheus Pompeo <matheuspompeo13@gmail.com>
2024-02-05 10:15:18 -0300

    05-02-24

---
+++ curso-tecnico/3o-ano/programacao-de-jogos/05-02-24/01/array_01.js
@@ -0,0 +1,16 @@
+let cidade = []
+
+console.log(cidade)
+
+cidade[0] = 'Tijucas'
+cidade[1] = 'Xique-Xique'
+cidade[2] = 'Chapecó'
+console.log(cidade.length)
+console.log('cidade = ' + cidade.join(', '))
+
+console.log(cidade.indexOf('Xique-Xique'))
+console.log(cidade.indexOf(''))
+
+let roupas = new Array()
+let roupas2 = new Array('Sapato', 'camiseta', 'tênis',)
+console.log(`Array1 = ${roupas}, Array2 = ${roupas2}`)
