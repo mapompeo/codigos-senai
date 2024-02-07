409aec0001c55951094a91e2841dfc6c7ccf0061
Matheus Pompeo <matheuspompeo13@gmail.com>
2024-02-07 16:34:04 -0300

    07-02-24

--- aprendizagem-industrial/fundamentos-de-eletronica/07-02-24/conversor.js
+++ aprendizagem-industrial/fundamentos-de-eletronica/07-02-24/conversor.js
@@ -1,7 +1,7 @@
 let resistividade = 0.0172 // 0,0172 Ω. mm²/m
-let areaTransversal = 1 // o valor mínimo é 1 para não dividir por zero
-let comprimento = 0
+let areaTransversal = 2.5 // o valor mínimo é 1 para não dividir por zero
+let comprimento = 1555
 
 let resistencia = (resistividade * comprimento) / areaTransversal
 
-console.log(`O valor da resistencia é de: ${resistencia.tofixed(2)}Ω`)
+console.log(`O valor da resistencia é de: ${resistencia}Ω`)
