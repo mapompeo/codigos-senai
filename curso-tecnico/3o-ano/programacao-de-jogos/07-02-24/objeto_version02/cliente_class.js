a5293a44583678060fd74888fdffec0c0db941f8
Matheus Pompeo <matheuspompeo13@gmail.com>
2024-02-07 10:14:48 -0300

    06-02-24

---
+++ curso-tecnico/3o-ano/programacao-de-jogos/07-02-24/objeto_version02/cliente_class.js
@@ -0,0 +1,11 @@
+class Pessoa {
+    constructor(nome, idade, altura) {
+        this.nome = nome
+        this.idade = idade
+        this.altura = altura
+    }
+    
+    mensagem() {
+        console.log("Olá")
+    }
+}
---
+++ curso-tecnico/3o-ano/programacao-de-jogos/07-02-24/objeto_version02/cliente_prog.js
@@ -0,0 +1,18 @@
+let resposta = document.getElementById('resposta')
+let cadastrar = document.getElementById('cadastrar')
+
+cadastrar.addEventListener('click', ()=> {
+    let nome = document.getElementById('nome').value
+    let idade = Number(document.getElementById('idade').value)
+    let altura = Number(document.getElementById('altura').value)
+
+    const cliente1 = new Pessoa()
+
+    cliente1.nome = nome
+    cliente1.idade = idade
+    cliente1.altura = altura
+
+    console.log(cliente1)
+
+    cliente1.mensagem()
+})
