0c5932d35827ac2a79bdaaf89ec2d7d79afd2401
Matheus Pompeo <matheuspompeo13@gmail.com>
2024-02-19 09:45:38 -0300

    carro

--- curso-tecnico/3o-ano/programacao-de-jogos/19-02-24/f1/f1_class.js
+++ curso-tecnico/3o-ano/programacao-de-jogos/19-02-24/f1/f1_class.js
@@ -1,4 +1,4 @@
-class Obj {
+class Objeto {
     constructor(x,y,w,h,a){
         this.x = x
         this.y = y
@@ -8,16 +8,39 @@ class Obj {
     }
 
     desenha_Objeto() {
-
+        des.fillStyle = this.a
+        des.fillRect(this.x,this.y,this.w,this.h)
     }
 }
 
-class carro extends Obj {
+class Carro extends Objeto {
+    dir = 0
+
     desenhaCarro(){
 
     }
 
     atualizaCarro(){
+        this.x = this.dir
+        if (this.x <= 2) {
+            this.dir = 2
+        }
+        else if (this.x >= 488 - this.w) {
+            this.dir = 488 - this.w
+        }
+    }
+}
 
+class Estrada extends Objeto {
+    desenhaEstrada() {
+        des.fillStyle = this.a
+        des.fillRect(this.x,this.y,this.w,this.h)
+    }
+
+    moveEstrada() {
+        this.y += 10
+        if (this.y >= 790) {
+            this.y = -100
+        }
     }
 }
--- curso-tecnico/3o-ano/programacao-de-jogos/19-02-24/f1/f1_main.js
+++ curso-tecnico/3o-ano/programacao-de-jogos/19-02-24/f1/f1_main.js
@@ -1,12 +1,41 @@
 let des = document.getElementById('des').getContext('2d')
-let carro = new Obj (10,10,100,100,'blue')
+
+let carro = new Carro (200,200,100,100,'black')
+let estradaEsquerda = new Estrada (2,0,10,696,'yellow')
+let estradaDireita = new Estrada (488,0,10,696,'yellow')
+let estradaCentral = new Estrada (246,20,10,80,'white')
+
+document.addEventListener('keydown', (e) => {
+    console.log(e.key)
+    if (e.key === 'a' || e.key === 'ArrowLeft') {
+        carro.dir -= 30
+    }
+    else if (e.key === 'd' || e.key === 'ArrowRight') {
+        carro.dir += 30
+    }
+})
+
+document.addEventListener('keyup', (e) => {
+    console.log(e.key)
+    if (e.key === 'a' || e.key === 'ArrowLeft') {
+        carro.dir -= 0
+    }
+    else if (e.key === 'd' || e.key === 'ArrowRight') {
+        carro.dir += 0
+    }
+}) 
+
 
 function desenha() {
-
+    estradaEsquerda.desenhaEstrada()
+    estradaDireita.desenhaEstrada()
+    estradaCentral.desenhaEstrada()
+    carro.desenha_Objeto()
 }
 
 function atualiza() {
-
+    estradaCentral.moveEstrada()
+    carro.atualizaCarro()
 }
 
 function main(){
@@ -15,4 +44,4 @@ function main(){
     atualiza()
 }
 
-setInterval(main,10)
+setInterval(main,20)
