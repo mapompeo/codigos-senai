f923a79e801f656296fda24bceb38c8590630617
Matheus Pompeo <matheuspompeo13@gmail.com>
2024-02-26 09:43:46 -0300

    arrumando o carro

---
+++ curso-tecnico/3o-ano/programacao-de-jogos/formula-1/assets/script/f1_class.js
@@ -0,0 +1,144 @@
+class Objeto {
+    constructor(x,y,w,h,a){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.a = a
+    }
+
+    desenha_Objeto() {
+        des.fillStyle = this.a
+        des.fillRect(this.x,this.y,this.w,this.h)
+    }
+}
+
+class Carro extends Objeto {
+    dirX = 0
+    dirY = 0
+    pontuacao = 0
+    vida = 3
+
+    desenhaCarro(){
+        // RODA DIANTEIRA DIREITA
+        des.beginPath()
+        des.lineWidth = '5'
+        des.strokeStyle = 'black'
+        des.fillStyle = 'black'
+        des.rect(this.x+15, this.y-60,10,10)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+        // RODA DIANTEIRA ESQUERDA
+        des.beginPath()
+        des.lineWidth = '5'
+        des.strokeStyle = 'black'
+        des.fillStyle = 'black'
+        des.rect(this.x-45, this.y-60,10,10)
+        des.closePath()
+        des.stroke()
+        des.fill()
+        
+        // RODA TRASEIRA ESQUERDA
+        des.beginPath()
+        des.lineWidth = '5'
+        des.strokeStyle = 'black'
+        des.fillStyle = 'black'
+        des.rect(this.x-45, this.y+50,10,10)
+        des.closePath()
+        des.stroke()
+        des.fill()
+            
+        // RODA TRASEIRA DIREITA
+        des.beginPath()
+        des.lineWidth = '5'
+        des.strokeStyle = 'black'
+        des.fillStyle = 'black'
+        des.rect(this.x+15, this.y+50,10,10)
+        des.closePath()
+        des.stroke()
+        des.fill()
+
+         // CARCAÇA
+         des.beginPath()
+         des.lineWidth = '5'
+         des.fillStyle = this.a
+         des.rect(this.x-30, this.y-60,40,120)
+         des.closePath()
+         des.stroke()
+         des.fill()
+    }
+
+    atualizaCarro(){
+        this.x += this.dirX
+        this.y += this.dirY
+
+        // esquerda
+        if (this.x <= 70) {
+            this.x = 70 
+        }
+        // direita
+        else if (this.x >= 464) {
+            this.x = 484
+        }
+        // cima
+        if (this.y <= 70) {
+            this.y = 70
+        // baixo
+        } else if (this.y >= 700 - this.h) {
+            this.y = 700 - this.h
+        }
+    }
+
+    point(objeto) {
+        if (this.h > objeto.h) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    colid(objeto) {
+        if ((this.x < objeto.x + objeto.w)&&(this.x + this.w > objeto.x)&&(this.y < objeto.y + objeto.h)&&(this.y + this.h > objeto.y)){
+            return true
+        }
+    }
+}
+
+class Carro02 extends Carro { 
+    atualizaCarro02() {
+        this.y += 3
+        if (this.y >= 700) {
+            this.y -= 800
+            this.x = Math.floor(Math.random() * (416 - 65 + 1) + 65)
+        }
+    }
+
+    recomeca() {
+        this.y = +700
+    }
+}
+
+class Estrada extends Objeto {
+    desenhaEstrada() {
+        des.fillStyle = this.a
+        des.fillRect(this.x,this.y,this.w,this.h)
+    }
+
+    moveEstrada() {
+        this.y += 10
+        if (this.y >= 790) {
+            this.y = -100
+        }
+    }
+}
+
+class Text {
+    desenhaTexto (text, x, y, cor, font) {
+       des.fillStyle = cor
+       des.lineWidth = '5'
+       des.font = font
+       des.fillText(text, x, y)
+    }
+}
---
+++ curso-tecnico/3o-ano/programacao-de-jogos/formula-1/assets/script/f1_main.js
@@ -0,0 +1,174 @@
+// API CANVAS
+let des = document.getElementById('des').getContext('2d')
+// FIM API CANVAS
+
+
+// VARIAVEIS
+let carro = new Carro (200,400,30,80,'red')
+let carro02 = new Carro02 (400,900,80,80,'blue')
+let carro03 = new Carro02 (132,1200,80,80,'green')
+let carro04 = new Carro02 (328,1500,80,80,'yellow')
+let carro05 = new Carro02 (5,1800,80,80,'orange')
+
+let estradaFundo = new Estrada ("./assets/images/asfalto.jpg")
+let estradaEsquerda = new Estrada (0,0,16,999,'white')
+let estradaDireita = new Estrada (484,0,16,999,'white')
+let estradaCentral01 = new Estrada (240,50,10,150,'white')
+let estradaCentral02 = new Estrada (240,330,10,150,'white')
+let estradaCentral03 = new Estrada (240,650,10,150,'white')
+
+let texto01 = new Text
+let texto02 = new Text
+
+let theme = new Audio("./assets/sounds/themeMusic.wav")
+let motor = new Audio("./assets/sounds/somCarro.wav")
+let batida = new Audio("./assets/sounds/batida.wav")
+// FIM VARIAVEIS
+
+
+// AUDIO
+theme.play()
+motor.play()
+theme.volume = 0.3
+theme.loop = true
+motor.volume = 0.3
+motor.loop = true
+batida.volume = 0.3
+// FIM AUDIO
+
+
+let jogar = true
+
+
+// COMEÇO CODIGO
+document.addEventListener('keydown', (e) => {
+    console.log(e.key)
+    if (e.key === 'a' || e.key === 'ArrowLeft') {
+        carro.dirX -= 10
+    }
+    else if (e.key === 'd' || e.key === 'ArrowRight') {
+        carro.dirX += 10
+    }
+    else if(e.key === 'w' || e.key === 'ArrowUp') {
+        carro.dirY -= 10
+    }
+    else if (e.key === 's' || e.key === 'ArrowDown') {
+        carro.dirY += 10
+    }
+    })
+
+document.addEventListener('keyup', (e) => {
+    if (e.key === 'a' || e.key === 'ArrowLeft') {
+        carro.dirX = 0
+        }
+    else if (e.key === 'd' || e.key === 'ArrowRight') {
+        carro.dirX = 0
+        }
+    else if (e.key === 'w' || e.key === 'ArrowUp') {
+        carro.dirY = 0
+        } 
+    else if (e.key === 's' || e.key === 'ArrowDown') {
+        carro.dirY = 0
+        }
+    })
+
+function pontos() {
+    if (carro.point(carro02)) {
+        carro.pontuacao += 1
+    }
+    else if (carro.point(carro03)) {
+        carro.pontuacao += 1
+    }
+    else if (carro.point(carro04)) {
+        carro.pontuacao += 1
+    } 
+    else if (carro.point(carro05)) {
+        carro.pontuacao += 1
+    }
+}
+
+function colisao() {
+    if (carro.colid(carro02)) {
+        carro.vida -= 1
+        carro02.recomeca()
+        batida.play()
+    } 
+    else if (carro.colid(carro03)) {
+        carro.vida -=1
+        carro03.recomeca()
+        batida.play()
+    }
+    else if (carro.colid(carro04)) {
+        carro.vida -= 1
+        carro04.recomeca()
+        batida.play()
+    }
+    else if (carro.colid(carro05)) {
+        carro.vida -= 1
+        carro05.recomeca()
+        batida.play()
+    }
+}
+        
+function desenha() {
+    if (jogar) {
+        estradaFundo.desenhaEstrada()
+        estradaEsquerda.desenhaEstrada()
+        estradaDireita.desenhaEstrada()
+        estradaCentral01.desenhaEstrada()
+        estradaCentral02.desenhaEstrada()
+        estradaCentral03.desenhaEstrada()
+
+        carro.desenhaCarro()
+        carro02.desenhaCarro()
+        carro03.desenhaCarro()
+        carro04.desenhaCarro()
+        carro05.desenhaCarro()
+    } else {
+        estradaFundo.desenhaEstrada()
+        estradaEsquerda.desenhaEstrada()
+        estradaDireita.desenhaEstrada()
+        estradaCentral01.desenhaEstrada()
+        estradaCentral02.desenhaEstrada()
+        estradaCentral03.desenhaEstrada()
+        texto01.desenhaTexto("GAME OVER!", 120, 350, 'red', '40px Montserrat')
+    }
+
+    texto01.desenhaTexto("Pontos:", 350, 40, 'white', '20px Montserrat')
+    texto01.desenhaTexto(carro.pontuacao, 440, 40, 'white', '20px Montserrat')
+
+    texto02.desenhaTexto("Vidas:", 40, 40, 'white', '20px Montserrat')
+    texto02.desenhaTexto(carro.vida, 120, 40, 'white', '20px Montserrat')
+}
+
+function atualiza() {
+    if (jogar) {
+        estradaCentral01.moveEstrada()
+        estradaCentral02.moveEstrada()
+        estradaCentral03.moveEstrada()
+        carro05.atualizaCarro02()
+        carro04.atualizaCarro02()
+        carro03.atualizaCarro02()
+        carro02.atualizaCarro02()
+        carro.atualizaCarro()
+        pontos()
+        colisao()
+        gameOver()
+    }
+}
+
+function main(){
+    des.clearRect(0,0,500,700)
+    desenha()
+    atualiza()
+}
+
+function gameOver() {
+    if (carro.vida <= 0) {
+        jogar = false
+        motor.pause()
+    }
+}
+
+setInterval(main,15)
+// FIM AUDIO
