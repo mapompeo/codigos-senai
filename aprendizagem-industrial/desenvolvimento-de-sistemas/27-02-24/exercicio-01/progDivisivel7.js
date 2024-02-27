a1f743af2591dd2af6d8938e7c7affe5f2400ba0
Matheus Pompeo <matheuspompeo13@gmail.com>
2024-02-27 14:58:52 -0300

    27-02

--- aprendizagem-industrial/desenvolvimento-de-sistemas/27-02-24/exercicio-01/progDivisivel7.js
+++ aprendizagem-industrial/desenvolvimento-de-sistemas/27-02-24/exercicio-01/progDivisivel7.js
@@ -1,24 +1,44 @@
-let resposta2 = document.getElementById('resposta2')
 let resposta = document.getElementById('resposta')
+let resposta2 = document.getElementById('resposta2')
+let resposta3 = document.getElementById('resposta3')
 let encontrar = document.getElementById('encontrar')
 let armazenar = document.getElementById('armazenar')
-
-let arrayTotal = []
+let valores = []
+let arrayDivisivel5 = []
 let arrayDivisivel7 = []
 
-armazenar.addEventListener('click', ()=> {
+armazenar.addEventListener('click', () => {
     resposta.innerHTML = ``
     let divisivel7 = Number(document.getElementById('inputUser').value)
 
-    arrayTotal.push(divisivel7)
-    resposta.innerHTML = `Os valores inseridos são: ${arrayTotal.join(', ')}.`
+    valores.push(divisivel7)
+    resposta.innerHTML = `Os valores inseridos são: ${valores.join(', ')}.`
 })
 
-encontrar.addEventListener('click', ()=> {
-    for (let i = 0; i < arrayTotal.length; i++) {
-        if(arrayTotal[i] % 7 == 0) {
-            arrayDivisivel7.push(arrayTotal[i])
+reiniciar.addEventListener('click', () => {
+    valores = []
+    divisivel = []
+    resposta.innerHTML = ``
+    resposta2.innerHTML = ``
+    resposta3.innerHTML = ``
+})
+
+divisivel7.addEventListener('click', () => {
+    resposta2.innerHTML = ` `
+    for (let i = 0; i < valores.length; i++) {
+        if(valores[i] % 7 == 0) {
+            arrayDivisivel7.push(valores[i])
         }
     }
     resposta2.innerHTML = `Os valores divisíveis por 7 são: ${arrayDivisivel7.join(', ')}.`
+})
+
+divisivel5.addEventListener('click', () => {
+    resposta3.innerHTML = ` `
+    for (let i = 0; i < valores.length; i++) {
+        if(valores[i] % 5 == 0) {
+            arrayDivisivel5.push(valores[i])
+        }
+    }
+    resposta3.innerHTML = `Os valores divisíveis por 5 são: ${arrayDivisivel5.join(', ')}.`
 })
