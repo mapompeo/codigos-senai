946dd60e4f5111b3b973876f514e1df08d8aa78f
Matheus Pompeo <matheuspompeo13@gmail.com>
2024-05-07 16:48:07 -0300

    07-05-24 aprendizagem

---
+++ aprendizagem-industrial/desenvolvimento-de-sistemas/07-05-24/pessoa/src/main/java/com/developer/pessoa/PessoaApplication.java
@@ -0,0 +1,20 @@
+package com.developer.pessoa;
+
+import com.developer.pessoa.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.mostraMenu();
+	}
+}
